5a3d655474008800bbd18a7da6ca0570738d153e
Sukma Arbianto <devbianberkah@gmail.com>
2023-12-27 00:15:51 +0700

    first Api

--- be/app/Http/Controllers/Api/LoginController.php
+++ be/app/Http/Controllers/Api/LoginController.php
@@ -5,8 +5,13 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Resources\LoginResource;
+use App\Models\User;
 
 class LoginController extends Controller
 {
     //
+    public function index(){
+        $logs = User::latest()->paginate(5);
+        return new LoginResource(true,'List User',$logs);
+    }
 }
