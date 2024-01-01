754dd743ee2c07d6fbb57e1a0f60f83fd0dba1f8
Sukma Arbianto <devbianberkah@gmail.com>
2024-01-01 15:53:14 +0700

    standarisasi output API

--- be/app/Http/Controllers/Api/LoginController.php
+++ be/app/Http/Controllers/Api/LoginController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Http\Resources\LoginResource;
+use App\Http\Resources\AcceptedRequestResource;
 use App\Http\Resources\CommonErrorResource;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
@@ -15,7 +15,7 @@ class LoginController extends Controller
     //
     public function index(){
         $logs = User::latest()->paginate(5);
-        return new LoginResource(true,'List User',$logs);
+        return new AcceptedRequestResource(true,'List User',$logs);
     }
 
     public function store(Request $request) {
@@ -25,20 +25,24 @@ class LoginController extends Controller
         ]);
 
         if ($validator->fails()){
-            return new CommonErrorResource(false,'ERR_INVALID_CREDS',$validator->errors());
+            // return ;
+            return response()->json(new CommonErrorResource(false,'ERR_INPUT',$validator->errors()), 401);
         }
 
         $user = User::where('email', $request->email)->first();
-        $data['user'] = $user;
-        // $data['access_token'] = $user->createToken($request->email)->plainTextToken;
-        // $data['refresh_token'] = $user->createToken($request->email)->plainTextToken;
-
-        return new LoginResource(true, 'Data Post Berhasil Ditambahkan!', $data);
-        // if (!user || Hash::check($request->password,$user->password)){
-        //     throw ValidationException::withMessages([
-        //         'email' => 'Salah password'
-        //     ]);
-        // }
-      
+        if ( $user)
+        {
+            if (!Hash::check($request->password,$user->password)){
+                return response()->json(new CommonErrorResource(false,'ERR_INVALID_CREDENTIAL','Wrong Password!'), 200);
+            }
+            $token = $user->createToken('authToken')->plainTextToken();
+            $data['user'] = $user;
+            $data['token'] = $token;
+            $cookie = Cookie::make($this->AUTH_COOKIE_NAME, $token, $this->COOKIE_EXPIRE_TIME);
+            return response()->json(new AcceptedRequestResource(true, 'Login Success!', $data), 200);   
+        }
+        else{
+            return response()->json(new CommonErrorResource(false,'ERR_INVALID_CREDENTIAL','User not found!'), 401);
+        }
     }
 }
